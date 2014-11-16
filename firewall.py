075b00cbc9989202ee2e990d8e5416671e62d0bb
Youny Kuang <loonyuni@gmail.com>
2014-11-15 21:00:26 -0800

    parse ipv4

--- firewall.py
+++ firewall.py
@@ -2,6 +2,9 @@
 
 from main import PKT_DIR_INCOMING, PKT_DIR_OUTGOING
 
+import struct # parse binary
+import socket 
+
 # TODO: Feel free to import any Python standard moduless as necessary.
 # (http://docs.python.org/2/library/)
 # You must NOT use any 3rd-party libraries, though.
@@ -41,8 +44,26 @@ class Firewall:
 
     # TODO: You can add more methods as you want.
     def parse_pkt(self, packet):
-        packet_info = dict()
+        pkt_info = dict()
         # IPv4 
+        header_valid = struct.unpack('B', packet[0])[0]
+        if (header_valid & 0b11000 == 4) and (header_valid & 0b0111) > 4: # not a valid packet
+             return None
+        fields = struct.unpack('!BBHHHBBHII', packet[:20])
+        pkt_info['v'] = fields[0] & 0b1100
+        pkt_info['hl'] = fields[0] & 0b0111
+        pkt_info['tos'] = fields[1]
+        pkt_info['len'] = fields[2]
+        pkt_info['id'] = fields[3]
+        pkt_info['flags'] = fields[4] >> 13 # right shift operator
+        pkt_info['off'] = fields[4] & 0x1fff
+        pkt_info['ttl'] = fields[5]
+        pkt_info['p'] = fields[6]
+        pkt_info['sum'] = fields[7]
+        pkt_info['src'] = socket.inet_ntoa(fields[8])
+        pkt_info['dst'] = socket.inet_ntoa(fields[9])
+
+        return pkt_info
 
     def packet_valid(self, pkt_dir, pkt):
         
